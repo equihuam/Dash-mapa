475807a717c6a00a518500d49330be4b0ceca044
Miguel Eq <equihuam@gmail.com>
2024-10-04 03:08:51 -0600

    Dash

--- app.R
+++ app.R
@@ -1,85 +1,83 @@
 library(shiny)    # for shiny apps
 library(leaflet)  # renderLeaflet function
 library(sf)
-library(shinydashboardPlus)
-library(shinydashboard)
 library(terra)
 library(tidyterra)
 library(ggplot2)
 library(sys)
 library(bs4Dash)
+library(shinydashboardPlus)
+library(shinydashboard)
 library(dplyr)
-library(fresh)
 
-dir_base <- ifelse (grepl("MONSTRUO", Sys.getenv()["USERDOMAIN"]),
-                    "D:/1 Nubes/El Instituto de Ecología/Proyecto Integralidad Gamma - Documentos/", 
-                    "C:/Users/equih/1 Nubes/OneDrive - El Instituto de Ecología/Documentos - Proyecto Integralidad Gamma/")
-dir_shp <- paste0(dir_base, "03 Documentos en preparación/02 Libro UNAM/mapas/")
+#dir_base <- ifelse (grepl("MONSTRUO", Sys.getenv()["USERDOMAIN"]),
+#                    "D:/1 Nubes/El Instituto de Ecología/Proyecto Integralidad Gamma - Documentos/", 
+#                    "C:/Users/equih/1 Nubes/OneDrive - El Instituto de Ecología/Documentos - Proyecto Integralidad Gamma/")
+#dir_shp <- paste0(dir_base, "03 Documentos en preparación/02 Libro UNAM/mapas/")
+#"Mex-IIE-ZVH-Albers/Capas"
 
-mapas_iie <- list.files(paste0(dir_shp, "Mex-IIE-ZVH-Albers/Capas/Estados/"),
+mapas_iie_r <- list.files("./Estados/",
                         full.names = TRUE, 
                         pattern = ".tif$")
-mapa_iie_muni_r <- 100 * rast(mapas_iie[30])
 
-mapas_iie_muni_v <- list.files(paste0(dir_shp, 
-                                      "Mex-IIE-ZVH-Albers/Capas/Estados/"),
+mapas_iie_muni_v <- list.files("./Estados/",
                                pattern = ".gpkg$",
                                full.names = TRUE)
 
+mapa_ver <- vect(mapas_iie_muni_v[30])
+mapa_ver$IIE_2018_mean <- 100 * mapa_ver$IIE_2018_mean
 bins<-c(0,12.5,25,37.5,50, 62.5,75,87.5, 100,Inf)
-cal <- colorBin(palette="RdYlGn", domain = mapa_ver$IIE_2018_mean., bins=bins)
-
-mapa_muni_v |> 
-  filter((IIE_2018_mean <= 30) &
-         (IIE_2018_mean > 20)) |> 
-  plot("IIE_2018_mean")
-
-ggplot(tibble(x = values(mapa_iie_muni_r)), 
-       aes(x = x, y = ..density..)) + 
-  geom_histogram(fill = cal(seq(0, 100, by=2)), 
-                 binwidth = 2, 
-                 na.rm = TRUE, 
-                 color="grey", 
-                 show.legend = FALSE) +
-  scale_fill_brewer(palette = "RdYlGn")
+cal <- colorBin(palette="RdYlGn", domain = mapa_ver$IIE_2018_mean., 
+                bins=bins)
 
 edos_lista <- unlist(lapply(mapas_iie_muni_v, function(x) 
   sub(".gpkg", "", basename(x))))
 
 ui = dashboardPage(
       title = "Promedios Municipales",
-      header = dashboardHeader(title = "Integridad Ecosistémica"),
+      header = dashboardHeader(title = "Integridad Ecosistémica",
+                               titleWidth = 280),
       
-      sidebar = dashboardSidebar(collapsed = FALSE, width = 350, 
+      sidebar = dashboardSidebar(collapsed = FALSE, width = 280, 
                         selectInput(inputId = "estado", 
                                     "Elige la entidad", 
                                     choices = edos_lista,
                                     selected = "Veracruz de Ignacio de la Llave"),
                         sliderInput(inputId = "iie_min",
-                                    "Mínima Integridad Ecosistémica:",
+                                    "Límite mínimo:",
                                     0, 100, value = 10),
                         sliderInput(inputId = "iie_max",
-                                    "Máxima Integridad Ecosistémica:",
+                                    "Límite máximo:",
                                     0, 100, value = 70),
                         box(title = "Lugar", width = 12,
                             verbatimTextOutput("muni")),
-                        box(width = 12, height = 150, 
-                            plotOutput("iie_h", width = 250, 
+                        box(width = 12, height = 170, 
+                            plotOutput("iie_h", width = 200, 
                                        height = 150)),
                       
                         tags$head(tags$style("#muni{color: blue;
                                               font-size: 14px;
                                               font-style: bold;}"))),
       
-      body = dashboardBody(
-                      fluidRow(box(title = textOutput("edo_tit"),
-                                   status = "primary", 
-                                   solidHeader = TRUE,
-                                   width = 12, 
-                                   height = 660,
-                                   leafletOutput(outputId = "map",
-                                                 width = 600, 
-                                                 height = 600)))))
+      body = dashboardBody(tabBox(width = 660,
+                      tabPanel("Municipios",
+                        fluidRow(       
+                           box(title = textOutput("edo_tit1"),
+                                       solidHeader = TRUE,
+                                       width = 12, 
+                                       height = 590,
+                                       leafletOutput(outputId = "map",
+                                                     width = 530, 
+                                                     height = 530)))),
+                      tabPanel("Raster",
+                        fluidRow(
+                           box(title = textOutput("edo_tit2"),
+                                       solidHeader = TRUE,
+                                       width = 12, 
+                                       height = 590,
+                                       leafletOutput(outputId = "map_r",
+                                                     width = 530, 
+                                                     height = 530)))))))
 
 server = function(input, output, session) {
 
@@ -104,18 +102,24 @@ server = function(input, output, session) {
                   smoothFactor = 0.03)
       })
   
+  output$map_r <- renderLeaflet({
+    leaflet() |> 
+    addRasterImage(mapa_r(), colors = cal)})  
+  
   output$iie_h <- renderPlot({
-    ggplot(tibble(x = values(mapa_iie_muni_r)), 
+    ggplot(tibble(x = values(mapa_r())), 
            aes(x = x, y = ..density..)) + 
       geom_histogram(fill = cal(seq(0, 100, by=2)), 
                      binwidth = 2, 
                      na.rm = TRUE, 
                      color="grey", 
                      show.legend = FALSE) +
-      scale_fill_brewer(palette = "RdYlGn")})
+      scale_fill_brewer(palette = "RdYlGn") + 
+      geom_vline(xintercept = mapa()$IIE_2018_mean[mapa()$id == input$map_shape_click$id])})
 
   # Cambio de mapa
-  output$edo_tit <- renderText({input$estado})
+  output$edo_tit1 <- renderText({input$estado})
+  output$edo_tit2 <- renderText({input$estado})
 
   mapa <- reactive({
             mapa <- vect(mapas_iie_muni_v[grepl(input$estado, 
@@ -124,8 +128,11 @@ server = function(input, output, session) {
               project(WGS84) |> 
               mutate(IIE_2018_mean = IIE_2018_mean * 100,
                      id = 1:n()) |>
-              st_as_sf()
-                      })
+              st_as_sf()})
+            
+  mapa_r <- reactive({
+              mapa_r <- 100 * rast(mapas_iie_r[grepl(input$estado, 
+                                                     mapas_iie_r)])})
                                                    
   #click on polygon
   observeEvent(input$map_shape_click, {
